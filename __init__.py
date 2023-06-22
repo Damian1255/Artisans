a6337100f9c60e2175104b3d9c2bc97e7891beef
Damian1255 <damianlau96@gmail.com>
2023-06-22 17:26:29 +0800

    Login validation

--- __init__.py
+++ __init__.py
@@ -10,17 +10,20 @@ log = logging.getLogger('werkzeug')
 app.secret_key = 'nani'
 salt = "wubba lubba dub dub"
 
-#disables flask logging
+# disables flask logging
 log.disabled = True
 app.logger.disabled = True
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/2')
 def index2():
     return render_template('index-2.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -30,28 +33,31 @@ def login():
             user_list = db['Users']
             db.close()
 
-            username = request.form['username']
-            #hashes password for comparison
-            password = hashlib.md5((request.form['password'] + salt).encode()).hexdigest()
+            username = request.json['username']
+            # hashes password for comparison
+            password = hashlib.md5(
+                (request.json['password'] + salt).encode()).hexdigest()
 
             for key in user_list:
                 if user_list[key].get_username() == username and user_list[key].get_password() == password:
-                    print(f"User {user_list[key].get_username()} has logged in successfully!")
+                    print(f"User {user_list[key].get_username()} logged in successfully!")
 
-                    #creates session
+                    # creates session
                     session['user_id'] = user_list[key].get_user_id()
                     session['first_name'] = user_list[key].get_first_name()
                     session['last_name'] = user_list[key].get_last_name()
                     session['username'] = user_list[key].get_username()
                     session['logged_in'] = True
 
-                    return redirect(url_for('index'))
+                    return jsonify({'success': True})
+            return jsonify({'success': False})
         except:
-            print("Error in retrieving Users from storage.db.")  
-            
+            print("Error in retrieving Users from storage.db.")
+
     return render_template('login.html')
 
-@app.route('/register', methods=['POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         # retrieves user list from storage.db
@@ -69,8 +75,9 @@ def register():
         username = request.json['username']
         email = request.json['email']
         # hash password
-        password = hashlib.md5((request.json['password'] + salt).encode()).hexdigest()
-        
+        password = hashlib.md5(
+            (request.json['password'] + salt).encode()).hexdigest()
+
         # checks if username or email is already taken
         available = True
         for key in user_list:
@@ -89,15 +96,16 @@ def register():
             db['Users'] = user_list
             db.close()
 
-            print(f"User created successfully! Username: {user.get_username()} Password: {user.get_password()} Email: {user.get_email()}")
+            print(
+                f"User created successfully! Username: {user.get_username()} Password: {user.get_password()} Email: {user.get_email()}")
             return jsonify({
                 'available': available,
                 'success': True
             })
-        
+
     return render_template('login.html', show_reg=True)
 
-            
+
 @app.route('/logout')
 def logout():
     # removes session
@@ -109,6 +117,7 @@ def logout():
 
     return redirect(url_for('index'))
 
+
 @app.route('/account')
 def account():
     if session['logged_in'] == True:
@@ -116,9 +125,11 @@ def account():
     else:
         return redirect(url_for('login'))
 
+
 @app.route('/admin')
 def admin():
     return render_template('admin/index.html')
+
 
 def get_user_list():
     try:
@@ -130,6 +141,7 @@ def get_user_list():
     except:
         user_list = {}
     return user_list
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/form-validation.js
+++ static/js/form-validation.js
@@ -1,21 +1,21 @@
 var registerForm = document.getElementById("register");
+var loginForm = document.getElementById("login");
 
 // validate register form
 registerForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    var first_name = registerForm.querySelector("input[name='first-name']").value;
-    var last_name = registerForm.querySelector("input[name='last-name']").value;
-    var email = registerForm.querySelector("input[name='email']").value;
-    var username = registerForm.querySelector("input[name='username']").value;
-    var password = registerForm.querySelector("input[name='password']").value;
-    var rePassword = registerForm.querySelector("input[name='re-password']").value;
+    var first_name = registerForm.querySelector("input[name='first-name']");
+    var last_name = registerForm.querySelector("input[name='last-name']");
+    var email = registerForm.querySelector("input[name='email']");
+    var username = registerForm.querySelector("input[name='username']");
+    var password = registerForm.querySelector("input[name='password']");
+    var rePassword = registerForm.querySelector("input[name='re-password']");
 
-    if (password != rePassword) {
-        registerForm.querySelector("input[name='password']").style.borderColor = "red";
-        registerForm.querySelector("input[name='re-password']").style.borderColor = "red";
-
-        show_reg_error("Passwords do not match!")
+    if (password.value != rePassword.value) {
+        // highlight password fields
+        highlight_elements_red([password, rePassword]);
+        show_reg_error("Passwords do not match.")
         return;
     }
 
@@ -29,25 +29,68 @@ registerForm.addEventListener("submit", function (event) {
             var response = JSON.parse(xhr.responseText);
 
             if (response.available && response.success) {
-                alert('Registration successful!');
+                registerForm.reset()
                 window.location.href = "/login";
             } else {
-                show_reg_error("Email or Username already exists!")
+                show_reg_error("Email or Username already exists.")
             }
         } else {
-            alert('Error registering.');
+            alert('An Internal error occurred.');
         }
     };
     xhr.send(JSON.stringify({
-        first_name: first_name,
-        last_name: last_name,
-        email: email,
-        username: username,
-        password: password
+        first_name: first_name.value,
+        last_name: last_name.value,
+        email: email.value,
+        username: username.value,
+        password: password.value
+    }));
+});
+
+// validate login form
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var username = loginForm.querySelector("input[name='username']");
+    var password = loginForm.querySelector("input[name='password']");
+
+    // Ajax call to login user 
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/login', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+
+            if (response.success) {
+                window.location.href = "/";
+            } else {
+                show_login_error("Invalid username or password.")
+                highlight_elements_red([username, password]);
+            }
+        } else {
+            alert('An Internal error occurred.');
+        }
+    };
+    xhr.send(JSON.stringify({
+        username: username.value,
+        password: password.value
     }));
 });
 
 function show_reg_error(message) {
     var msgBox = document.getElementById("reg-msg-box");
     msgBox.innerHTML = message;
+}
+
+function show_login_error(message) {
+    var msgBox = document.getElementById("login-msg-box");
+    msgBox.innerHTML = message;
+}
+
+function highlight_elements_red(element_arr) {
+    element_arr.forEach(element => {
+        element.style.borderColor = "red";
+    });
 }
